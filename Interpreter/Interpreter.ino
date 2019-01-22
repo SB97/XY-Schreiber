38f20514d2d6d14a3ab5ac4a6fd089ebe2823824
Steffen Bauer <steffen97.bauer@gmail.com>
2019-01-22 11:51:34 +0100

    fast alles übernommen

--- Interpreter/Interpreter.ino
+++ Interpreter/Interpreter.ino
@@ -1,7 +1,7 @@
 /*
- * https://www.marginallyclever.com/2013/08/how-to-build-an-2-axis-arduino-cnc-gcode-interpreter/
- * Der Interpreter soll per Serieller verbindung GCODE empfangen und auswerten.
- */
+   https://www.marginallyclever.com/2013/08/how-to-build-an-2-axis-arduino-cnc-gcode-interpreter/
+   Der Interpreter soll per Serieller verbindung GCODE empfangen und auswerten.
+*/
 
 // Konstanten
 #define BAUD (57600) // Baudrate 
@@ -13,10 +13,38 @@ int bufferAuslastung; // belegung der zwischenablage
 void setup() {
   // Serielle verbindung starten
   Serial.begin(BAUD);
-
+  Serial.print(F("Start!"));
+  ready();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/*
+   Vorbereitung auf neuen Serielen Input
+*/
+void ready() {
+  bufferAuslastung = 0; // Nachrichtenspeicher leeren
+  Serial.print(F("> ")); // output um zu sehen wann der Arduino bereit für einen Befehl ist
+}
 
+/*
+ * GCODE empfangen und weiterleiten
+ */
+void loop() {
+  // empfangen
+  if( Serial.available() ) {
+    char c = Serial.read(); // empfang speichern
+    Serial.print("habe '" + String(c) + "' empfangen!"); 
+    
+    // speichern
+    if( bufferAuslastung < MAX_BUF ) {
+      buffer[bufferAuslastung++] = c; // ????????????????????????
+      }
+
+    // Nachrichten ende
+    if( c == '\n' ) {
+      Serial.print("Befehl ende!");
+      buffer[bufferAuslastung] = 0;
+      // WEITER VERARBEITUNG!!!
+      ready();
+      }
+    }
 }
